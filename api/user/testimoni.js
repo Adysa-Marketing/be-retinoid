ab9b4ffb2f9f88de766fe9138ce939b8aee079d0
Redha <tataredhaalfath@gmail.com>
2023-06-23 14:51:06 +0700

    feat:user-testimonial

---
+++ api/user/testimoni.js
@@ -0,0 +1,27 @@
+const { Testimonial } = require("../../models");
+const logger = require("../../libs");
+
+module.exports = async (req, res) => {
+  const source = req.body;
+  try {
+    const payload = {
+      userId: source.userId,
+      rating: source.rating,
+      testimonial: source.testimonial,
+      remark: source.remark,
+    };
+
+    logger.info({ source, payload });
+    await Testimonial.create(payload);
+    return res.status(201).json({
+      status: "success",
+      message: "Terimakasih atas review yang diberikan",
+    });
+  } catch (error) {
+    console.log("[!] Error : ", error);
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
