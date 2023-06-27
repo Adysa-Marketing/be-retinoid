fb468e7f015685f345adc5ead42da33bfd1329e9
Redha <tataredhaalfath@gmail.com>
2023-06-27 12:09:28 +0700

    .

--- api/v1/setting/user/index.js
+++ api/v1/setting/user/index.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
+const Asset = require("./asset");
 const ChangePass = require("./changepassword");
 const Get = require("./get");
 const Update = require("./update");
+const Upload = require("../../../../libs/upload");
 
 router.get("/get/:id", Get);
 router.put("/change-pass", ChangePass);
-router.put("/update", Update);
+router.put(
+  "/update",
+  Asset.Directory,
+  Upload.fields([{ name: "image", maxCount: 1 }]),
+  Update
+);
 
 module.exports = router;
