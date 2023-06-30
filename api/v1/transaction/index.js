dcd8f698c7ea305bf2fd7368bd835cecb42df9fa
Redha <tataredhaalfath@gmail.com>
2023-06-30 10:33:49 +0700

    enhance trx:statistic

--- api/v1/transaction/index.js
+++ api/v1/transaction/index.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 const Mutation = require("./mutation");
 const Product = require("./product");
 const Reward = require("./reward");
+const Statistic = require("./statistic");
 const Stokis = require("./stokis");
 const Widhraw = require("./widhraw");
 
 router.use("/mutation", Mutation);
 router.use("/product", Product);
 router.use("/reward", Reward);
+router.use("/stat", Statistic);
 router.use("/stokis", Stokis);
 router.use("/widhraw", Widhraw);
 
--- api/v1/transaction/statistic/index.js
+++ api/v1/transaction/statistic/index.js
@@ -2,28 +2,28 @@ const router = require("express").Router();
 const Statistic = require("./statistic");
 
 // admin
-router.get("/stat", Statistic.ChartSales);
-router.get("/stat/product", Statistic.Product);
-router.get("/stat/agen", Statistic.Agen);
-router.get("/stat/member", Statistic.Member);
-router.get("/stat/package", Statistic.Package);
-router.get("/stat/stokis", Statistic.Stokis);
-router.get("/stat/sale", Statistic.TrSale);
-router.get("/stat/income", Statistic.Income);
-router.get("/stat/mon-income", Statistic.MonthlyIncome);
-router.get("/stat/sale-stokis", Statistic.TrStokis);
-router.get("/stat/income-stokis", Statistic.IncomeTrStokis);
-router.get("/stat/mon-income-stokis", Statistic.MonthlyIncomeTrStokis);
-router.get("/stat/mon-rw", Statistic.MonthlyTrReward);
-router.get("/stat/mon-wd", Statistic.MonthlyWd);
-router.get("/stat/mon-spend-wd", Statistic.MonthlySpendingWd);
+router.get("", Statistic.ChartSales);
+router.get("/product", Statistic.Product);
+router.get("/agen", Statistic.Agen);
+router.get("/member", Statistic.Member);
+router.get("/package", Statistic.Package);
+router.get("/stokis", Statistic.Stokis);
+router.get("/sale", Statistic.TrSale);
+router.get("/income", Statistic.Income);
+router.get("/mon-income", Statistic.MonthlyIncome);
+router.get("/sale-stokis", Statistic.TrStokis);
+router.get("/income-stokis", Statistic.IncomeTrStokis);
+router.get("/mon-income-stokis", Statistic.MonthlyIncomeTrStokis);
+router.get("/mon-rw", Statistic.MonthlyTrReward);
+router.get("/mon-wd", Statistic.MonthlyWd);
+router.get("/mon-spend-wd", Statistic.MonthlySpendingWd);
 
 // agen + member
-router.get("/stat/agen-sale", Statistic.ATrSale);
-router.get("/stat/bonus", Statistic.Bonus);
-router.get("/stat/mon-bonus", Statistic.MonthlyBonus);
-router.get("/stat/success-wd", Statistic.SuccessWidhraw);
-router.get("/stat/refferal", Statistic.Refferals);
-router.get("/stat/tr-reward", Statistic.TrReward);
+router.get("/agen-sale", Statistic.ATrSale);
+router.get("/bonus", Statistic.Bonus);
+router.get("/mon-bonus", Statistic.MonthlyBonus);
+router.get("/success-wd", Statistic.SuccessWidhraw);
+router.get("/refferal", Statistic.Refferals);
+router.get("/tr-reward", Statistic.TrReward);
 
 module.exports = router;
