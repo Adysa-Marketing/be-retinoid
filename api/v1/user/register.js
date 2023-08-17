acbd51e14502a229d1ad27f16dd7105d23ce12f4
Redha <tataredhaalfath13@gmail.com>
2023-08-17 14:59:15 +0700

    integrate wabot for upliner

--- api/v1/user/register.js
+++ api/v1/user/register.js
@@ -15,6 +15,7 @@ const Op = sequelize.Op;
 const bcrypt = require("bcryptjs");
 const cryptoString = require("crypto-random-string");
 const moment = require("moment");
+const wabot = require("../../../libs/wabot");
 
 const Validator = require("fastest-validator");
 const v = new Validator();
@@ -24,6 +25,7 @@ module.exports = async (req, res) => {
   const transaction = await db.sequelize.transaction({ autocommit: false });
   let isRegistered = false;
   let upliner = null;
+  let dataNotification = [];
 
   try {
     const schema = {
@@ -164,11 +166,18 @@ module.exports = async (req, res) => {
     const commission = parseInt(
       (commissionLevel.percent * productAmount) / 100
     );
-    const message = `Selamat anda mendapatkan bonus senilai Rp.${new Intl.NumberFormat(
+    const userUpliner = await User.findOne({
+      attributes: ["id", "username", "phone"],
+      where: { id: sponsor.userId },
+    });
+    const message = `Hi ${
+      userUpliner.username
+    }, Selamat anda mendapatkan bonus senilai Rp.${new Intl.NumberFormat(
       "id-ID"
     ).format(commission)} dari downline ${
       userData.name
     } dengan kedalaman level 1`;
+
     await Commission.create(
       {
         userId: sponsor.userId,
@@ -180,6 +189,17 @@ module.exports = async (req, res) => {
       },
       { transaction }
     );
+
+    // set data notification for upliner
+    if (userUpliner && userUpliner.phone) {
+      const payloadNotif = {
+        to: userUpliner.phone,
+        message,
+      };
+      dataNotification.push(payloadNotif);
+    }
+    // end set data notification for upliner
+
     // create generation
     await Generation.create(
       {
@@ -201,15 +221,16 @@ module.exports = async (req, res) => {
 
     // end notifikasi wa bot
 
+    // proses commission
     await calculateDownlineBonus(
       sponsor.userId, // id penyeponsor
       productAmount, // 500.000
       2, // level
       userData.id, // id downline
       userData.name, // name of downline
+      dataNotification, // data notification
       transaction // db transactional
     );
-    // proses commission
 
     transaction.commit();
 
@@ -240,6 +261,10 @@ module.exports = async (req, res) => {
     }
   } finally {
     if (isRegistered && upliner != null) {
+      // send wa notification
+      for (const payload of dataNotification) {
+        await wabot.Send(payload);
+      }
       await calculatePoint(upliner);
     }
   }
@@ -251,6 +276,7 @@ const calculateDownlineBonus = async (
   level, // commission level
   downlineId, // id downline
   downlineName, // name of downline
+  dataNotification, // data notification
   transaction // db transactional
 ) => {
   if (level > 5) {
@@ -278,7 +304,13 @@ const calculateDownlineBonus = async (
     where: { id: level },
   });
   const commission = (commissionLevel.percent * amount) / 100;
-  const message = `Selamat anda mendapatkan bonus senilai Rp.${new Intl.NumberFormat(
+  const userUpliner = await User.findOne({
+    attributes: ["id", "username", "phone"],
+    where: { id: userUplineId },
+  });
+  const message = `Hi ${
+    userUpliner.username
+  }, Selamat anda mendapatkan bonus senilai Rp.${new Intl.NumberFormat(
     "id-ID"
   ).format(
     commission
@@ -296,6 +328,16 @@ const calculateDownlineBonus = async (
     },
     { transaction }
   );
+
+  // set data notification for upliner
+  if (userUpliner && userUpliner.phone) {
+    const payloadNotif = {
+      to: userUpliner.phone,
+      message,
+    };
+    dataNotification.push(payloadNotif);
+  }
+  // end set data notification for upliner
 
   // create generation
   await Generation.create(
@@ -322,6 +364,7 @@ const calculateDownlineBonus = async (
     level + 1,
     downlineId,
     downlineName,
+    dataNotification,
     transaction
   );
 };
