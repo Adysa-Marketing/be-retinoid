c56766e63b6248d640e47c05746f506409492f10
Redha <tataredhaalfath13@gmail.com>
2023-08-18 09:45:34 +0700

    enhance wabot auto sync

--- libs/wabot.js
+++ libs/wabot.js
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV;
 const config = require("../config/core")[env];
 const logger = require("./logger");
+const { WaBot } = require("../models");
 
 const axios = require("axios");
 
@@ -13,8 +14,21 @@ module.exports.Send = (payloadData) => {
     axios
       .post(`${config.apiBot}/api/send-chat`, payloadData, { headers })
       .then((res) => {
-        logger.info({ received: res.data.data });
-        resolve(res.data.data);
+        if (
+          res.data.status !== 200 &&
+          res.data.message == "Session tidak ditemukan"
+        ) {
+          console.log(
+            "[!] Error WABOT :",
+            res.data.message,
+            "- [WABOT RE-SYNC PROCESS]"
+          );
+          reSend(payloadData);
+          resolve(true);
+        } else {
+          logger.info({ received: res.data.data });
+          resolve(res.data.data);
+        }
       })
       .catch((err) => {
         logger.info(err);
@@ -22,3 +36,25 @@ module.exports.Send = (payloadData) => {
       });
   });
 };
+
+async function reSend(payloadData) {
+  try {
+    const wabot = await WaBot.findOne({ attributes: ["id", "key"] });
+    token = wabot.key;
+    const headers = {
+      token: token, // global var token
+    };
+    logger.info({ resending: payloadData, headers });
+    axios
+      .post(`${config.apiBot}/api/send-chat`, payloadData, { headers })
+      .then((res) => {
+        logger.info({ received: res.data.data });
+      })
+      .catch((err) => {
+        logger.info(err);
+        reject(err);
+      });
+  } catch (error) {
+    console.log("[!] Error : ", error);
+  }
+}
